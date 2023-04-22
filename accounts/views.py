3f3d6e350ada8317ba963a5d588b4403cd8e996a
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-04-22 18:15:37 +0330

    change database to posgresql and add accounts/models and forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import View
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'accounts/register.html')
